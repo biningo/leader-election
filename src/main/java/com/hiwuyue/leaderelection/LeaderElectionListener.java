ff753b3769c51e6bd9e87d055efd63302c389c1d
biningo <biningo.cn@gmail.com>
2024-07-22 23:37:20 +0800

    Add comments to leader election interface

--- src/main/java/com/hiwuyue/leaderelection/LeaderElectionListener.java
+++ src/main/java/com/hiwuyue/leaderelection/LeaderElectionListener.java
@@ -1,5 +1,13 @@
 package com.hiwuyue.leaderelection;
 
+/**
+ * This is the leader election listener used to listen to leader election results.
+ */
 public interface LeaderElectionListener {
+    /**
+     * After the leader election callback.
+     *
+     * @param isLeader whether this node is a leader.
+     */
     void onLeaderElect(boolean isLeader);
 }
--- src/main/java/com/hiwuyue/leaderelection/LeaderElector.java
+++ src/main/java/com/hiwuyue/leaderelection/LeaderElector.java
@@ -2,18 +2,53 @@ package com.hiwuyue.leaderelection;
 
 import java.util.List;
 
+/**
+ * This is a leader elector used to elect the leader node in the cluster.
+ */
 public interface LeaderElector {
+
+    /**
+     * Start watching the leader election.
+     */
     void startWatchElection();
 
+    /**
+     * Stop watching the leader election.
+     */
     void stopWatchElection();
 
+    /**
+     * Register the leader election listener.
+     *
+     * @param listener the listener listens to the election results.
+     */
     void registerListener(LeaderElectionListener listener);
 
+    /**
+     * Notify all listeners whether this node is a leader.
+     *
+     * @param isLeader whether this node is a leader.
+     */
     void notifyLeaderElect(boolean isLeader);
 
+    /**
+     * Whether this node is a leader.
+     *
+     * @return true if the node is leader, otw false.
+     */
     boolean isLeader();
 
+    /**
+     * Get the node election id.
+     *
+     * @return elector id.
+     */
     String getElectorId();
 
+    /**
+     * Get a list of all election peer ids.
+     *
+     * @return all election peer ids.
+     */
     List<String> getElectionPeers();
 }
